f6f78dfd99e924244af5ba73d8e1b1dc0486b03d
Saldruk <Divermeik@gmail.com>
2022-11-11 10:21:45 +0100

    Update Funktionen 1 und 2

--- script_01.js
+++ script_01.js
@@ -2,3 +2,25 @@
 
 /***** Funktionen 01 *****/
 
+// Kapselung von Codeblöcken
+
+//Funktionsaufruf (call)
+//test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo Max")
+}
+
+/*********** Funktionen 02a ***********/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+function ausgabeNamen() {
+    let firstname = "Meik";
+    console.log("Hallo "+ firstname + " !");
+    }
+    
